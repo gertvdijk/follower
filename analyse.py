2cf54c07b0cf7034aebcb79c53c9ee0414fafe94
Gert van Dijk <gertvdijk@gmail.com>
2022-08-21 01:33:35 +0200

    Format files using Black

--- analyse.py
+++ analyse.py
@@ -16,10 +16,10 @@ login = secrets.readlines()
 
 # assign the values accordingly
 # strip the linebreak from the values to prevent bad login errors
-consumer_key = login[0].rstrip('\n')
-consumer_secret = login[1].rstrip('\n')
-access_token = login[2].rstrip('\n')
-access_token_secret = login[3].rstrip('\n')
+consumer_key = login[0].rstrip("\n")
+consumer_secret = login[1].rstrip("\n")
+access_token = login[2].rstrip("\n")
+access_token_secret = login[3].rstrip("\n")
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -30,6 +30,7 @@ def save_json(variable, filename):
     with io.open(filename, "w", encoding="utf-8") as f:
         f.write(str(json.dumps(variable, indent=4, ensure_ascii=False)))
 
+
 def load_json(filename):
     ret = None
     if os.path.exists(filename):
@@ -39,6 +40,7 @@ def load_json(filename):
         except:
             pass
     return ret
+
 
 def try_load_or_process(filename, processor_fn, function_arg):
     load_fn = None
@@ -58,6 +60,7 @@ def try_load_or_process(filename, processor_fn, function_arg):
         save_fn(ret, filename)
         return ret
 
+
 # Some helper functions to convert between different time formats and perform date calculations
 def twitter_time_to_object(time_string):
     twitter_format = "%a %b %d %H:%M:%S %Y"
@@ -71,36 +74,44 @@ def twitter_time_to_object(time_string):
         date_object = datetime.strptime(new_string, twitter_format)
         return date_object
 
+
 def time_object_to_unix(time_object):
     return int(time_object.strftime("%s"))
 
+
 def twitter_time_to_unix(time_string):
     return time_object_to_unix(twitter_time_to_object(time_string))
+
 
 def seconds_since_twitter_time(time_string):
     input_time_unix = int(twitter_time_to_unix(time_string))
     current_time_unix = int(get_utc_unix_time())
     return current_time_unix - input_time_unix
 
+
 def get_utc_unix_time():
     dts = datetime.utcnow()
     return time.mktime(dts.timetuple())
 
+
 # Get a list of follower ids for the target account
 def get_follower_ids(target):
-    ids= []
+    ids = []
     for page in tweepy.Cursor(auth_api.get_follower_ids, screen_name=target).pages():
         ids.extend(page)
 
     return ids
+
 
 # Twitter API allows us to batch query 100 accounts at a time
 # So we'll create batches of 100 follower ids and gather Twitter User objects for each batch
 def get_user_objects(follower_ids):
     batch_len = 100
     num_batches = len(follower_ids) / 100
-    #num_batches = 243000 / 100
-    batches = (follower_ids[i:i+batch_len] for i in range(0, len(follower_ids), batch_len))
+    # num_batches = 243000 / 100
+    batches = (
+        follower_ids[i : i + batch_len] for i in range(0, len(follower_ids), batch_len)
+    )
     all_data = []
     for batch_count, batch in enumerate(batches):
         sys.stdout.write("\r")
@@ -110,23 +121,24 @@ def get_user_objects(follower_ids):
         sys.stdout.write("\n")
         sys.stdout.flush()
         users_list = auth_api.lookup_users(user_id=batch)
-        users_json = (map(lambda t: t._json, users_list))
+        users_json = map(lambda t: t._json, users_list)
         all_data += users_json
     return all_data
+
 
 # Creates one week length ranges and finds items that fit into those range boundaries
 def make_ranges(user_data, num_ranges=10):
     range_max = 604800 * num_ranges
-    range_step = range_max/num_ranges
+    range_step = range_max / num_ranges
 
-# We create ranges and labels first and then iterate these when going through the whole list
-# of user data, to speed things up
+    # We create ranges and labels first and then iterate these when going through the whole list
+    # of user data, to speed things up
     ranges = {}
     labels = {}
     for x in range(num_ranges):
         start_range = x * range_step
         end_range = x * range_step + range_step
-        label = "%02d" % x + " - " + "%02d" % (x+1) + " weeks"
+        label = "%02d" % x + " - " + "%02d" % (x + 1) + " weeks"
         labels[label] = []
         ranges[label] = {}
         ranges[label]["start"] = start_range
@@ -135,35 +147,47 @@ def make_ranges(user_data, num_ranges=10):
         if "created_at" in user:
             account_age = seconds_since_twitter_time(user["created_at"])
             for label, timestamps in ranges.items():
-                if account_age > timestamps["start"] and account_age < timestamps["end"]:
-                    entry = {} 
-                    id_str = user["id_str"] 
-                    entry[id_str] = {} 
-                    fields = ["screen_name", "name", "created_at", "friends_count", "followers_count", "favourites_count", "statuses_count"] 
-                    for f in fields: 
-                        if f in user: 
-                            entry[id_str][f] = user[f] 
-                    labels[label].append(entry) 
+                if (
+                    account_age > timestamps["start"]
+                    and account_age < timestamps["end"]
+                ):
+                    entry = {}
+                    id_str = user["id_str"]
+                    entry[id_str] = {}
+                    fields = [
+                        "screen_name",
+                        "name",
+                        "created_at",
+                        "friends_count",
+                        "followers_count",
+                        "favourites_count",
+                        "statuses_count",
+                    ]
+                    for f in fields:
+                        if f in user:
+                            entry[id_str][f] = user[f]
+                    labels[label].append(entry)
     return labels
+
 
 def get_follower_data(target):
     print("Processing target: " + target)
 
-# Get a list of Twitter ids for followers of target account and save it
+    # Get a list of Twitter ids for followers of target account and save it
     filename = target + "_follower_ids.json"
     follower_ids = try_load_or_process(filename, get_follower_ids, target)
 
-# Fetch Twitter User objects from each Twitter id found and save the data
+    # Fetch Twitter User objects from each Twitter id found and save the data
     filename = target + "_followers.json"
     user_objects = try_load_or_process(filename, get_user_objects, follower_ids)
     total_objects = len(user_objects)
 
-# Record a few details about each account that falls between specified age ranges
+    # Record a few details about each account that falls between specified age ranges
     ranges = make_ranges(user_objects)
     filename = target + "_ranges.json"
     save_json(ranges, filename)
 
-# Print a few summaries
+    # Print a few summaries
     res = "\n"
     res += "Follower age ranges\n"
     res += "===================\n"
--- followers.py
+++ followers.py
@@ -1,43 +1,45 @@
-from analyse import get_follower_data 
+from analyse import get_follower_data
 from check import check_tweeps
 import os
 from sys import platform
 
+
 def analyse_user():
-    print('Geef de twitter username (zonder @) in')
-    print('en de datum vanaf wanneer je informatie wilt')
-    print('over volgers. Let op de aangegeven datum notatie.')
-    print('Geef ook aan of je bestaande data wilt gebruiken,')
+    print("Geef de twitter username (zonder @) in")
+    print("en de datum vanaf wanneer je informatie wilt")
+    print("over volgers. Let op de aangegeven datum notatie.")
+    print("Geef ook aan of je bestaande data wilt gebruiken,")
     print('je kunt ook de data opnieuw gebruiken door "n" in te geven.\n\n')
 
-    wie = input('Twitter username: ')
-    datum = input('Vanaf welke datum (dd-mm-jjj): ')
-    reuse_data = input('Data opnieuw ophalen (j|n): ')
+    wie = input("Twitter username: ")
+    datum = input("Vanaf welke datum (dd-mm-jjj): ")
+    reuse_data = input("Data opnieuw ophalen (j|n): ")
 
-    if reuse_data == 'j' or reuse_data == 'J':
+    if reuse_data == "j" or reuse_data == "J":
         try:
-            if platform == 'linux' or platform == 'linux2' or platform == 'darwin':
-                os.system('rm *.json')
-            elif platform == 'win32':
-                os.system('del *.json')
+            if platform == "linux" or platform == "linux2" or platform == "darwin":
+                os.system("rm *.json")
+            elif platform == "win32":
+                os.system("del *.json")
         except:
-            print('No files to delete')
-    elif reuse_data == 'n' or reuse_data == 'N':
-        print('We gebruiken de bestaande data indien aanwezig.')
+            print("No files to delete")
+    elif reuse_data == "n" or reuse_data == "N":
+        print("We gebruiken de bestaande data indien aanwezig.")
     else:
-        print('Graag j of n ingeven. Voer het script opnieuw uit.')
+        print("Graag j of n ingeven. Voer het script opnieuw uit.")
         sys.exit()
 
     followers = get_follower_data(wie)
     test = check_tweeps(wie, datum)
 
-    if platform == 'linux' or platform == 'linux2' or platform == 'darwin':
-        os.system('clear')
-    elif platform == 'win32':
-        os.system('cls')
+    if platform == "linux" or platform == "linux2" or platform == "darwin":
+        os.system("clear")
+    elif platform == "win32":
+        os.system("cls")
 
     print(followers)
-    print('\n\n')
+    print("\n\n")
     print(test)
+
 
 analyse_user()
